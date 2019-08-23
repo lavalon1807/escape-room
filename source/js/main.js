3896b356c98d8737f2e71278e3bede70d6990e6d
Egor <Cost_benefit@mail.ru>
2019-08-23 17:30:42 +0300

    интерактивность главной страницы, мобильной и десктопной

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,59 @@
 var button = document.querySelector('.page-header__toggle');
-var modalCity = document.querySelector('.modal-city__show');
+var mainNav = document.querySelector('.main-nav');
+var stats = document.querySelector('.stats');
+var footer = document.querySelector('.page-footer');
+var bain = document.querySelector('.bain');
+var bainDesktop = document.querySelector('.bain__desktop');
+var divBain = document.querySelector('div');
 
+// button.addEventListener('click', function () {
+//      divBain.classList.remove('bain');
+//  })
+
+
+button.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav__closed')) {
+    divBain.classList.remove('bain');
+
+    mainNav.classList.remove('main-nav__closed');
+    mainNav.classList.add('main-nav__opened');
+
+    stats.classList.add('stats__closed');
+    stats.classList.remove('stats__opened');
+
+    footer.classList.remove('page-footer__closed');
+    footer.classList.add('page-footer__opened');
+
+    bainDesktop.classList.remove('bain__desktop--closed');
+    bainDesktop.classList.add('bain__desktop--opened');
+
+    button.classList.remove('page-header__toggle--closed');
+    button.classList.add('page-header__toggle--opened');
+
+    // button.classList.add('page-header__toggle--closed');
+    // button.classList.add('page-header__toggle--opened::after');
+
+
+  } else {
+    divBain.classList.add('bain');
+
+    mainNav.classList.add('main-nav__closed');
+    mainNav.classList.remove('main-nav__opened');
+
+    stats.classList.remove('stats__closed');
+    stats.classList.add('stats__opened');
+
+    footer.classList.add('page-footer__closed');
+    footer.classList.remove('page-footer__opened');
+
+    bainDesktop.classList.add('bain__desktop--closed');
+    bainDesktop.classList.remove('bain__desktop--opened');
+
+    button.classList.add('page-header__toggle--closed');
+    button.classList.remove('page-header__toggle--opened');
+
+    // button.classList.remove('.page-header__toggle--closed::before');
+    // button.classList.add('page-header__toggle--closed::before');
+  }
+})
 
